ee2c4f4e3ed56630ebebfe8a774ee482116d62bc
CrisM93 <criztian.gio@gmail.com>
2022-12-20 13:54:05 -0600

    Comentario

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+//Dos Actividades, en RAMA....
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.twoactivities.extra.MESSAGE";
